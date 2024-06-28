4d084f9eff40d866d7e54d05c05c8d67bad26c52
lalmeidaribeiro <larissa191@gmail.com>
2024-06-28 20:12:23 -0300

    finalizado

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# jogos
+<h1 align="center"> Repositório com alguns jogos treinando JavaScript! </h1>
+
+<p>Este repositório foi criado com o objetivo de treinar o uso de conceitos básicos de JavaScript, tais como variáveis, condicionais, estruturas de repetição, arrays e funções.</p>
+
+<p>Para isso, utilizei cinco projetos distintos disponibilizados na plataforma Alura para treinamento.</p>
+
+<p>O primeiro projeto a ser desenvolvido é o "Sorteador de Números".</p>
--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,33 +1,44 @@
-//Precisa criar uma informações que sortea os números das label Qntidado, do núemor e até o número
-//document - busca itens dentro do HTML - usar document.FUNÇÃO 
-//getElementById() - busca elementos do HTML pelo ID documento.getElementId('ID DO ESTAMSO BUSCANDO')
-
-//Dar alertas para saber se a variavel está sendo exibida - Nessa faze conseguimos ver so o usuário digirou na label
-  /** alert(`Quantidade: ${quantidade}`);
-    alert(`Do número: ${de}`);
-    alert(`Até o número: ${ate}`); */
-    //Se mostrar, significa que conseguimos recuperar os números do intervalo
-
-// Para forçar que um avariavel seja do tipo numerico adicionameos partseInt() andes do document
- /* 
- Sortear o número aleatório - Criamos uma função para gerar os números aleatórios e adicionamos ela a funcition SORTEAR
-    Passamos o parametro min(de) e max(ate)
-    function obterNumeroAleatorio(min, max)
-
-    Criamos uma variavel número que recebe a função obterNumeroAleatorio e passamos os parametros das variaveis da function sortear
-    let numerico - obterNumeroAleatorio(de, ate);
- 
- */
 
 function sortear(){
     let quantidade = parseInt(document.getElementById('quantidade').value);
     let de = parseInt(document.getElementById('de').value);
     let ate = parseInt(document.getElementById('ate').value);
+    let sorteados = [];
+    let numero; 
+    let resultado;
 
-    let numero = obterNumeroAleatorio(de, ate);
-    alert(numero)
+    for(let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio(de, ate);
+
+        while(sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de, ate);
+        }
+        sorteados.push(numero);
+    }
+    resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados} </label>`;
+    alterarStaursBotao();
 }
 
 function obterNumeroAleatorio(min, max){
-    return Math.random() * (max - min) + min;
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function alterarStaursBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStaursBotao();
 }
