633bf9975947942ffd99d6e6a5a2754a2833412c
laalmeidaribeiro <rlarissa191@gmail.com>
2024-07-04 08:16:00 -0300

    confirmacao para devolver o jogo

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,24 +1,22 @@
-function alterarStatus(id){
-  let gameclicado = document.getElementById(`game-${id}`);
-  let imagem = gameclicado.querySelector('.dashboard__item__img');
-  let botao = gameclicado.querySelector('.dashboard__item__button');
+function alterarStatus(id) {
+  let jogoClicado = document.getElementById(`game-${id}`);
+  let imagem = jogoClicado.querySelector('.dashboard__item__img');
+  let botao = jogoClicado.querySelector('.dashboard__item__button');
+  let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
 
-  if(imagem.classList.contains('dashboard__item__button--return')){
-      imagem.classList.remove('dashboard__item__button--return');
+  if(imagem.classList.contains('dashboard__item__img--rented')){
+    //confirm é aler com yes or no
+    if(confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)){
+      imagem.classList.remove('dashboard__item__img--rented');
+      botao.classList.remove('dashboard__item__button--return');
+      botao.textContent = 'Alugar';
+    }  
   }else{
-      imagem.classList.add('dashboard__item__button--return');
+    imagem.classList.add('dashboard__item__img--rented');
+    botao.textContent = 'Devolver';
+    botao.textContent.add('dashboard__item__button--return');
+
   }
-
-
 }
 
 
-
-/*
-if(statusId != 1 && statusId !=2){
-  alert('Funciona jogo 03');
-}else if(statusId !=2 && statusId !=3){
-  alert('Funciona jogo 01');
-}else{
-  alert('Funciona jogo 02');
-}*/
