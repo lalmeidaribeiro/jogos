8e2f9fa9b7242b2ce2b70eaf8e45a37adfa16dec
laalmeidaribeiro <rlarissa191@gmail.com>
2024-07-04 08:29:49 -0300

    imprimindo no console numero de jogos alugados

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,3 +1,10 @@
+let jogosAlugados = 0;
+
+//função para imprimir no console a informação sobre quantos jogos foram alugados.
+function imprimiJogosAlugados(){
+  console.log(`Total de jogos alugados: ${jogosAlugados}`);
+}
+
 function alterarStatus(id) {
   let jogoClicado = document.getElementById(`game-${id}`);
   let imagem = jogoClicado.querySelector('.dashboard__item__img');
@@ -5,7 +12,7 @@ function alterarStatus(id) {
   let nomeJogo = jogoClicado.querySelector('.dashboard__item__name');
 
   if(imagem.classList.contains('dashboard__item__img--rented')){
-    //confirm é aler com yes or no
+    //confirm é alerta com s/n
     if(confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)){
       imagem.classList.remove('dashboard__item__img--rented');
       botao.classList.remove('dashboard__item__button--return');
@@ -15,8 +22,14 @@ function alterarStatus(id) {
     imagem.classList.add('dashboard__item__img--rented');
     botao.textContent = 'Devolver';
     botao.textContent.add('dashboard__item__button--return');
-
   }
+  jogosAlugados++;
+  imprimiJogosAlugados();
 }
 
+// Inicializa a contagem considerando que os jogos já começam alugados
+document.addEventListener('DOMContentLoaded', function() {
+  jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+  contarEExibirJogosAlugados();
+});
 
