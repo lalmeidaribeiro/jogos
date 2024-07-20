415aeefc8bd5561a7eea8ec89cb5e67ce3addf09
laalmeidariberi <rlarissa191@gmail.com>
2024-07-19 23:54:02 -0300

    function embalha nomes sorteados

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,6 +1,9 @@
+let sorteados = [];
+
 function adicionar(){
     let nomeSorteio = document.getElementById('nome-amigo').value;
     let listaAmigos = document.getElementById('lista-amigos');
+    sorteados.push(nomeSorteio);
 
     if(listaAmigos.textContent == ''){
         listaAmigos.textContent = nomeSorteio;
@@ -8,5 +11,17 @@ function adicionar(){
         listaAmigos.textContent = listaAmigos.textContent +', '+ nomeSorteio;
     }
     document.getElementById('nome-amigo').value = '';
+}
+
+function sortear(){
+    embaralhaSorteio(sorteados);
+}
+
+//Algoritimo de Fisher-Yates embaralha o sorteio
+function embaralhaSorteio(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
 
