292bb6a9969618d961409f1a840ed8933c687921
laalmeidariberi <rlarissa191@gmail.com>
2024-07-19 22:12:48 -0300

    fnction superior e inferior

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -4,20 +4,45 @@ function comprar(){
 
     if(tipo.value == 'pista'){
         comprarPista(qntReservada);
-
+    }else if(tipo.value == 'superior'){
+        comprarSuperior(qntReservada);
+    }else{
+        comprarInferior(qntReservada);
     }
 }
 
-function comprarPista(qntReservada){
+function comprarPista(qntReservadaPista){
     let qntPista = parseInt(document.getElementById("qtd-pista").textContent);
 
-    if (qntReservada > qntPista){
+    if (qntReservadaPista > qntPista){
         alert('Quantidade indisponivel para o tipo de pista');
     }else{
-        qntPista = qntPista - qntReservada;
-    
+        qntPista = qntPista - qntReservadaPista;
         document.getElementById("qtd-pista").textContent = qntPista;
         alert('Parabéns, compra realizadacom sucesso!');
-        
+    }
+}
+
+function comprarSuperior(qntReservadaSuperior){
+    let qntSuperior = parseInt(document.getElementById("qtd-superior").textContent);
+
+    if (qntReservadaSuperior > qntSuperior){
+        alert('Quantidade indisponivel para o tipo de superior');
+    }else{
+        qntSuperior = qntSuperior - qntReservadaSuperior;
+        document.getElementById("qtd-superior").textContent = qntSuperior;
+        alert('Parabéns, compra realizadacom sucesso!');
+    }
+}
+
+function comprarInferior(qntReservadaInferior){
+    let qntInferior = parseInt(document.getElementById("qtd-inferior").textContent);
+
+    if (qntReservadaInferior > qntInferior){
+        alert('Quantidade indisponivel para o tipo de inferior');
+    }else{
+        qntInferior = qntInferior - qntReservadaInferior;
+        document.getElementById("qtd-inferior").textContent = qntInferior;
+        alert('Parabéns, compra realizadacom sucesso!');
     }
 }
